5c7d3c45964366aa17cb5b8fedd409211e38c80e
gilesv <viniciusgcp@gmail.com>
2018-06-26 22:19:01 -0300

    configuring react

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,26 @@
 const path = require("path");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: "./src/index.js",
     devtool: "inline-source-map",
+    
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+        ]
+    },
     devServer: {
         contentBase: "./docs"
     },
     output: {
         filename: "main.js",
-        path: path.resolve(__dirname, "docs")
+        path: path.resolve(__dirname, "docs/scripts")
     }
 }
