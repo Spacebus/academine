7b6e0f2bfbcc1e3ec2f0aeeda85da3cdbcf5dc3b
gilesv <viniciusgcp@gmail.com>
2018-07-02 13:56:00 -0300

    backoffice

--- src/components/Backoffice.js
+++ src/components/Backoffice.js
@@ -5,7 +5,13 @@ export default class Backoffice extends React.Component {
     render() {
         return (
             <App>
-                <div>Backoffice</div>
+                <div className="backoffice">
+                    <h1>Backoffice</h1>
+                    <p>Inserir dados</p>
+                    <input type="file" id="xml" name="xml" accept=".xml" multiple />
+                    <button onClick={() => sendData()}>Enviar</button>
+                </div>
+                
             </App>
         )
     }
