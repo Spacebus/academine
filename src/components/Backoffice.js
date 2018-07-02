91660cc8c192de636a1fd9aab9f7bd4097fb34fc
gilesv <viniciusgcp@gmail.com>
2018-07-02 17:26:43 -0300

    backoffice

--- src/components/Backoffice.js
+++ src/components/Backoffice.js
@@ -2,14 +2,47 @@ import React from "react";
 import App from "./App.js";
 
 export default class Backoffice extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            files: []
+        }
+        this.files = [];
+        this.handleFileSelect = this.handleFileSelect.bind(this);
+
+    }
+
+    handleFileSelect() {
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+            var files = document.getElementById("xml").files; // FileList object
+
+            // Loop through the FileList and render image files as thumbnails.
+            for (var i = 0, f; f = files[i]; i++) {
+
+                var reader = new FileReader()
+
+                // Closure to capture the file information.
+                reader.onload = (e) => {
+                    this.files.push(e.target.result);
+                };
+
+                // Read in the image file as a data URL.
+                reader.readAsText(f, "ISO-8859-1");
+            }
+            console.log( this.files );
+          } else {
+            alert('The File APIs are not fully supported in this browser.');
+          }
+    }
+
     render() {
         return (
             <App>
                 <div className="backoffice">
                     <h1>Backoffice</h1>
                     <p>Inserir dados</p>
-                    <input type="file" id="xml" name="xml" accept=".xml" multiple />
-                    <button onClick={() => sendData()}>Enviar</button>
+                    <input type="file" id="xml" name="xml"  multiple onChange={() => this.handleFileSelect()} />
+                    <button>Enviar</button>
                 </div>
                 
             </App>
